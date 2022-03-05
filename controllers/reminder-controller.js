daabc3c8ae0f0263a379d8e31f04a97bc11f0282
RaveenaWickramasinghe <raveenawickramasinghe@gmail.com>
2022-03-05 11:22:21 +0530

    implemented get notifications service file

--- controllers/reminder-controller.js
+++ controllers/reminder-controller.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const createNotificationController = require("../services/createReminder-service");
+const viewNotificationsbyId = require("../services/viewReminders-service");
+const viewAllNotifications = require("../services/viewReminders-service");
 
 module.exports = function () {
   router.post("/create", createNotificationController.createReminder);
+  router.get("/view/:id", viewNotificationsbyId.viewNotificationById);
+  router.get("/", viewAllNotifications.viewNotifications);
   return router;
 };
