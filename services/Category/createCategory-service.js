b3ddfc11b8bcaca44f2d5d1e340346478ea9f51d
Mithma Goonawardena <mithma.vs@gmail.com>
2022-03-22 04:01:09 +0530

    save category

--- services/Category/createCategory-service.js
+++ services/Category/createCategory-service.js
@@ -3,48 +3,41 @@ const Category = require("../../models/category-model");
 const createCategory = async (req, res) => {
   if (req.body) {
     let category = new Category();
-    const container = req.body;
-    console.log(container.userId);
+    const container = req.body.data;
+ 
 
     let arr = container.data;
-    console.log(arr);
 
-    Object.entries(arr).forEach(([key, value]) => {
-      console.log(key, value);
 
+
+    let count = Object.entries(arr).length;
+
+    let x = 0;
+    for(const [key, value] of Object.entries(arr)){
+      x++;
       let category = new Category({
-        userId: container.userId,
         categoryName: key,
         categoryColor: value,
       });
-      category
+
+      
+      await category
         .save()
         .then((data) => {
-          res.status(200).send({ data: data });
-        })
+          if(x==count){
+            return res.status(200).send({ data: data });
+          }
+        }
+        )
         .catch((error) => {
-          res.status(500).send({ error: error.message });
+          return res.status(500).send({ error: error.message });
+          
         });
-    });
+      
+      
+    }
 
-    //const category =
 
-    //const arr = [req.body];
-    // const catergories = new Category();
-    // catergories = container.map((item) => ({
-    //   token: item.token,
-    //   categoryColor: item.data.categoryColor[0],
-    //   categoryName: item.data.categoryName[1],
-    // }));
-
-    // await catergories
-    //   .save()
-    //   .then((data) => {
-    //     res.status(200).send({ data: data });
-    //   })
-    //   .catch((error) => {
-    //     res.status(500).send({ error: error.message });
-    //   });
   }
 };
 
