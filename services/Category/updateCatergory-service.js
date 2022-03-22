a7bf460201074643f89d90d20ac298a0ac34bd55
Ashini98K <ashini.kulatunga@gmail.com>
2022-03-22 12:34:10 +0530

    Implemented catergory update

--- services/Category/updateCatergory-service.js
+++ services/Category/updateCatergory-service.js
@@ -5,32 +5,23 @@ const updateCategory = async (req, res) => {
   console.log(userId);
 
   if (userId) {
-    let category = new Category();
     const container = req.body;
 
-    let arr = container,
-      data;
-    console.log(container);
+    let arr = container.data;
 
+    let count = Object.entries(arr).length;
     let x = 0;
+
     for (const [key, value] of Object.entries(arr)) {
       x++;
-      let category = new Category({
-        userId: container.id,
-        categoryName: key,
-        categoryColor: value,
-      });
 
       await Category.findOneAndUpdate(
-        { categoryColor: req.body.categoryColor },
+        { categoryColor: key },
         {
           $set: {
-            //userId: req.body.userId,
-            categoryName: req.body.categoryName,
-            //categoryColor: req.body.categoryColor,
+            categoryName: value,
           },
         }
-        //{ upsert: true }
       )
         .then((data) => {
           if (x == count) {
@@ -41,24 +32,6 @@ const updateCategory = async (req, res) => {
           return res.status(500).send({ error: error.message });
         });
     }
-
-    // await Category.findOneAndUpdate(
-    //   { categoryColor: req.body.categoryColor },
-    //   {
-    //     $set: {
-    //       //userId: req.body.userId,
-    //       categoryName: req.body.categoryName,
-    //       //categoryColor: req.body.categoryColor,
-    //     },
-    //   }
-    //   //{ upsert: true }
-    // )
-    //   .then((data) => {
-    //     res.status(200).send({ data: data });
-    //   })
-    //   .catch((error) => {
-    //     res.status(500).send({ error: error.message });
-    //   });
   }
 };
 
