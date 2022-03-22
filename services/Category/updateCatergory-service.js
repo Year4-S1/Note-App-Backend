488d131bef6507df0757fec7d79ee2c84bde4891
Ashini98K <ashini.kulatunga@gmail.com>
2022-03-22 11:18:12 +0530

    did changes

--- services/Category/updateCatergory-service.js
+++ services/Category/updateCatergory-service.js
@@ -1,27 +1,33 @@
 const Category = require("../../models/category-model");
 
 const updateCategory = async (req, res) => {
-  let userToken = await Category.find({ token: req.params.id });
-  console.log(userToken);
+  let userId = await Category.find({ userId: req.params.id });
+  console.log(userId);
 
-  if (userToken) {
-    await Category.findOneAndUpdate(
-      { categoryColor: req.body.categoryColor },
-      {
-        $set: {
-          //token: req.body.token,
-          categoryName: req.body.categoryName,
-          //categoryColor: req.body.categoryColor,
-        },
-      }
-      //{ upsert: true }
-    )
-      .then((data) => {
-        res.status(200).send({ data: data });
-      })
-      .catch((error) => {
-        res.status(500).send({ error: error.message });
-      });
+  if (userId) {
+    let category = new Category();
+    const container = req.body.data;
+
+    let arr = container;
+    console.log(container);
+
+    // await Category.findOneAndUpdate(
+    //   { categoryColor: req.body.categoryColor },
+    //   {
+    //     $set: {
+    //       //userId: req.body.userId,
+    //       categoryName: req.body.categoryName,
+    //       //categoryColor: req.body.categoryColor,
+    //     },
+    //   }
+    //   //{ upsert: true }
+    // )
+    //   .then((data) => {
+    //     res.status(200).send({ data: data });
+    //   })
+    //   .catch((error) => {
+    //     res.status(500).send({ error: error.message });
+    //   });
   }
 };
 
