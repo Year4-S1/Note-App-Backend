e9666a017dd73894833d982e16a28d7bdc5bcad9
Ashini98K <ashini.kulatunga@gmail.com>
2022-03-25 22:52:03 +0530

    Implemented filter in category

--- services/Category/viewCategory-service.js
+++ services/Category/viewCategory-service.js
@@ -12,9 +12,20 @@ const viewCategory = async (req, res) => {
 
 const viewCategoryByUseId = async (req, res) => {
   var query = { userId: req.params.id };
+
   await Category.find({ userId: req.params.id })
+
     .then((data) => {
-      res.status(200).send({ data: data });
+      console.log(data.length);
+      let filterData = [];
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].categoryName != "") {
+          filterData.push(data[i]);
+        }
+      }
+      console.log(filterData);
+      const len = filterData.length;
+      res.status(200).send({ data: filterData });
     })
     .catch((error) => {
       res.status(500).send({ error: error.message });
