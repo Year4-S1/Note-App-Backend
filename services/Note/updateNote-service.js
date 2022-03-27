344ffaaf2aec1df1d6509947b19c6347be87f471
Ashini98K <ashini.kulatunga@gmail.com>
2022-03-27 20:25:00 +0530

    Did changes to to note update added a content type check

--- services/Note/updateNote-service.js
+++ services/Note/updateNote-service.js
@@ -1,30 +1,30 @@
-const read = require("body-parser/lib/read");
-const { callbackPromise } = require("nodemailer/lib/shared");
 const Note = require("../../models/note-model");
 
 const updateNote = async (req, res) => {
-  let datanew = {};
-
-  Note.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: {
-        categoryColor: req.body.categoryColor,
-        noteTitle: req.body.noteTitle,
-        noteMessage: req.body.noteMessage,
-        noteDate: new Date().toLocaleDateString(),
-        noteTime: new Date().toTimeString(),
+  if (!req.is("application/json")) {
+    res.send(400);
+  } else {
+    Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          categoryColor: req.body.categoryColor,
+          noteTitle: req.body.noteTitle,
+          noteMessage: req.body.noteMessage,
+          noteDate: new Date().toLocaleDateString(),
+          noteTime: new Date().toTimeString(),
+        },
       },
-    },
-    { new: true },
-    function (err, result) {
-      if (err) {
-        res.send(500, body);
-      } else {
-        res.send(200, result);
+      { upsert: true },
+      function (err, result) {
+        if (err) {
+          res.status(500).send(body);
+        } else {
+          res.status(200).send(result);
+        }
       }
-    }
-  );
+    );
+  }
 
   // console.log(datanew);
 
