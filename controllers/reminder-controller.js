4e341a765399d6b6c0b6fdc5e6d49e060e1af9e2
raveenaw_IT19146652 <it19146652@my.sliit.lk>
2022-03-28 19:44:23 +0530

    added category colour

--- controllers/reminder-controller.js
+++ controllers/reminder-controller.js
@@ -6,6 +6,8 @@ const viewNotificationsbyId = require("../services/Reminder/viewReminders-servic
 const viewAllNotifications = require("../services/Reminder/viewReminders-service");
 const deleteNotificationByID = require("../services/Reminder/deleteReminder-service");
 const updateReminder = require("../services/Reminder/updateReminder-service");
+const viewReminderByUserID = require("../services/Reminder/viewReminders-service");
+const viewReminderByCategoryId = require("../services/Reminder/viewReminders-service");
 
 module.exports = function () {
   router.post("/create", createNotificationController.createReminder);
@@ -13,6 +15,7 @@ module.exports = function () {
   router.get("/", viewAllNotifications.viewNotifications);
   router.put("/update/:id",updateReminder.updateReminder);
   router.get("/delete/:id", deleteNotificationByID.deleteNotification);
-
+  router.get("/userid/:id", viewReminderByUserID.viewReminderByUserId);
+  router.get("/category/:id", viewReminderByCategoryId.viewRemindersByCategoryId);
   return router;
-};
+}; 
