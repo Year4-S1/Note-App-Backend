1686dacf857823e309d087c3115c87540d190dd8
Mithma Goonawardena <mithma.vs@gmail.com>
2022-04-07 03:06:05 +0530

    changes

--- services/Favorite/get-favorites.js
+++ services/Favorite/get-favorites.js
@@ -11,7 +11,15 @@ const getFavorites = async (req, res) => {
     $and: [{ userId: uid }, { favoriteStatus: true }],
   }).populate({path:"noteId", strictPopulate:false})
     .then((data) => {
-      res.status(200).send({ data: data });
+      
+      let arr = [];
+
+      for(let i=0; i < data.length; i++){
+        arr.push(data[i].noteId)
+      }
+      console.log(arr)
+      // console.log(data[0].noteId)
+      res.status(200).send({ data: arr });
     })
     .catch((error) => {
       res.status(500).send({ error: error.message });
